ee135a8864471d55fd44424e88e621dd07b86ddf
mabdullahns007 <160305769+mabdullahns007@users.noreply.github.com>
2024-08-20 16:19:26 +0500

    Update app.py

--- Test/app.py
+++ Test/app.py
@@ -1,4 +1,10 @@
-
+import streamlit as st
+from langchain_community.document_loaders import YoutubeLoader
+from langchain_community.document_loaders.youtube import TranscriptFormat
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+import os
 
 import streamlit as st
 st.title("Meal planner")
