f472c019a094a402dfd60c0181f2e733617b81c1
mabdullahns007 <160305769+mabdullahns007@users.noreply.github.com>
2024-08-20 16:07:40 +0500

    Update app.py

--- Video Summarization/app.py
+++ Video Summarization/app.py
@@ -7,7 +7,7 @@ from langchain.chains.summarize import load_summarize_chain
 import os
 
 # Set your Google API key
-os.environ["GOOGLE_API_KEY"] = ""
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCqMCBk2k1-pbACD3grHQIpiK7NKiDEx4A"
 
 st.title("YouTube Video Summarizer")
 
