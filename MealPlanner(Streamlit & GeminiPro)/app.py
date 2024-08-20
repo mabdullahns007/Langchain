4eafda485a60b1915c23366cb02c66c7ed4b090c
mabdullahns007 <160305769+mabdullahns007@users.noreply.github.com>
2024-08-20 16:55:14 +0500

    Update app.py

--- MealPlanner(Streamlit & GeminiPro)/app.py
+++ MealPlanner(Streamlit & GeminiPro)/app.py
@@ -6,7 +6,7 @@ from google.colab import userdata
 from IPython.display import display
 from IPython.display import Markdown
 
-genai.configure(api_key="AIzaSyCqMCBk2k1-pbACD3grHQIpiK7NKiDEx4A")
+genai.configure(api_key="")
 
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
